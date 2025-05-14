3fe2742a52c7b614d3f58296d2e47bb569e29045
Nick Yaremovich <nickyaremovich@gmail.com>
2025-05-14 17:05:15 -0600

    complete 1-6

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    if (str === "") return "";
+    let arr = str.split("");
+    let reverse = arr.reverse();
+    let reverseString = reverse.join("");
+    return reverseString;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
