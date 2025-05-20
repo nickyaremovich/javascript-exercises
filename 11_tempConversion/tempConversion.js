c5090ef5212f220f5eeb1766da5368a03c1747d5
Nick Yaremovich <nickyaremovich@gmail.com>
2025-05-20 15:30:43 -0600

    Refactor functions and update tests for removeFromArray, sumAll, leapYears, and temperature conversion

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(f) {
+  let celsius = (f - 32) * 5/9  
+  return parseFloat(celsius.toFixed(1))
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  let fahrenheit = (c * 1.8) + 32
+  return parseFloat(fahrenheit.toFixed(1))
+
 };
 
 // Do not edit below this line
